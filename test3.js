50df5e557676c9998c4d2ac04d63af4109340d49
Dusty Little <dlittle@localhost.localdomain>
2013-05-29 23:58:42 -0500

    first attempt at html parsing

--- test3.js
+++ test3.js
@@ -1,13 +1,30 @@
 var request = require('request')
   , cheerio = require('cheerio');
 
-var ref = 'https://www.etsy.com/developers/documentation/#reference';
+var base = 'https://www.etsy.com';
+var ref = '/developers/documentation/#reference';
 
-function parseHTML(err, res, html) {
+request(base + ref, function (err, res, html) {
   if (err) return console.error(err);
   var phtml = cheerio.load(html);
-  var resrc = [];
-  phtml(
-}
+  var rescs = [];
+  phtml('div#reference ul li a').each(function() {
+    var $ = cheerio(this);
+    request(base + $.attr('href'), function (err, res, html) {
+      if (err) return console.error(err);
+      var phtml = cheerio.load(html);
+      phtml('div.docs-content').each(function() {
+        var $ = cheerio(this), resc = {};
+        resc['name'] = $.find('h1').text();
+        resc['fields'] = [];
+        $('table#resource_fields tr').each(function() {
+          $('td').first(function() {
+            resc['fields'].push({ name: $(this).text()});
+          });
+        });
+        console.log(resc);
+      });
+    });
+  });
+});
 
-http.request(options, callback).end();
